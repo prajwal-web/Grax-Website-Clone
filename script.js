a4dbf268dcb865377151225929abd57f4f1981b5
Prajwal V <prajuu2000@gmail.com>
2025-03-07 17:51:03 +0530

    feat: created home page

---
+++ script.js
@@ -0,0 +1,11 @@
+const animate = gsap.from(".animate-word", {
+  duration: 1,
+  opacity: 0,
+  x: 50,
+  stagger: 0.1,
+  ease: "power2.out",
+  delay: 0.2,
+  stagger: 0.1,
+  rotation: 10,
+  force3D: true,
+});

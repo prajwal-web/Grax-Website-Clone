05ba7fab2cd97aecba140eddad969dfbe6b5f16c
prajwal-web <prajuu2000@gmail.com>
2025-03-08 21:34:09 +0530

    feat: adding navbar animation

--- script.js
+++ script.js
@@ -9,3 +9,49 @@ const animate = gsap.from(".animate-word", {
   rotation: 10,
   force3D: true,
 });
+
+// gsap.from(".animate-word", {
+//   duration: 1.2, 
+//   opacity: 0,
+//   x: -150, 
+//   y: (index) => Math.sin(index * 0.8) * 30, 
+//   stagger: {
+//     amount: 1, 
+//     from: "start",
+//   },
+//   rotation: (index) => Math.sin(index * 0.8) * 20, 
+//   ease: "elastic.out(1, 0.75)", 
+//   delay: 0.1,
+//   force3D: true,
+// });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav ul li a');
+
+  navLinks.forEach(link => {
+      link.addEventListener('click', function (e) {
+          e.preventDefault(); 
+
+          const targetId = this.getAttribute('href').substring(1); 
+          const targetSection = document.getElementById(targetId); 
+
+          if (targetSection) {
+              targetSection.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'start'
+              });
+          }
+      });
+  });
+});
+
+gsap.from("#about", {
+  opacity: 0,
+  y: 50,
+  duration: 1,
+  scrollTrigger: {
+      trigger: "#about",
+      start: "top 80%",
+  },
+});
